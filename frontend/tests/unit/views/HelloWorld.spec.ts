9d79f7fa716f28dfc2beb50a0845261cf964429a
Lucas ONeil <lucasoneil@gmail.com>
2022-12-14 14:47:34 -0800

    Remove test for missing page
    Signed-off-by: Lucas ONeil <lucasoneil@gmail.com>

--- frontend/tests/unit/views/HelloWorld.spec.ts
+++ frontend/tests/unit/views/HelloWorld.spec.ts
@@ -1,8 +1,9 @@
 import { mount } from '@vue/test-utils';
-import HelloWorld from '@/views/HelloWorld.vue';
+// import HelloWorld from '@/views/HelloWorld.vue';
 
+// Skip until a real view test can be added in here
 describe('HelloWorld.vue', () => {
-  it('renders', async () => {
+  it.skip('renders', async () => {
     const wrapper = mount(HelloWorld);
     expect(wrapper.find('p').text()).toBe('Hello World!');
   });
