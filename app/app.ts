ba687d78a08738419581ae3f08f988d6c5c1400e
Dragan Colic <dragan.colic.ca@gmail.com>
2022-12-23 14:23:17 -0800

    Extending get/config endpoint to return idpList default or override configuration

--- app/app.ts
+++ app/app.ts
@@ -3,6 +3,9 @@ import compression from 'compression';
 import config from 'config';
 import path from 'path';
 
+const idplistDefaultPath = './config/idplist-default.json';
+const idplistOverridePath = './config/idplist-override.json';
+
 const app = express();
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
@@ -12,9 +15,10 @@ app.get('/', (req, res) => {
 });
 
 // Frontend configuration endpoint
-app.get('/config', (_req, res, next) => {
+app.get('/config', async (_req, res, next) => {
   try {
-    const frontend = config.get('frontend');
+    const frontend = { ...config.get('frontend') };
+    frontend.idpList = await readIdpList();
     res.status(200).json(frontend);
   } catch (err) {
     next(err);
@@ -27,5 +31,11 @@ app.use('/favicon.ico', (_req, res) => {
   res.redirect(`${staticFilesPath}/favicon.ico`);
 });
 app.use(staticFilesPath, express.static(path.join(__dirname, 'dist')));
+
+async function readIdpList(): Promise<string> {
+  return (await import(idplistOverridePath).catch(async e => {
+    return await import(idplistDefaultPath);
+  })).default;
+}
 
 export default app;
