3e975a558203666ab7c810dab502f5bbb74ff480
Dragan Colic <dragan.colic.ca@gmail.com>
2022-12-23 14:39:14 -0800

    Moving path constants into the function and moving the config read calls into the response

--- app/app.ts
+++ app/app.ts
@@ -3,9 +3,6 @@ import compression from 'compression';
 import config from 'config';
 import path from 'path';
 
-const idplistDefaultPath = './config/idplist-default.json';
-const idplistOverridePath = './config/idplist-override.json';
-
 const app = express();
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
@@ -17,9 +14,10 @@ app.get('/', (req, res) => {
 // Frontend configuration endpoint
 app.get('/config', async (_req, res, next) => {
   try {
-    const frontend = { ...config.get('frontend') };
-    frontend.idpList = await readIdpList();
-    res.status(200).json(frontend);
+    res.status(200).json({
+      ...config.get('frontend'),
+      idpList: await readIdpList()
+   });
   } catch (err) {
     next(err);
   }
@@ -33,6 +31,8 @@ app.use('/favicon.ico', (_req, res) => {
 app.use(staticFilesPath, express.static(path.join(__dirname, 'dist')));
 
 async function readIdpList(): Promise<string> {
+  const idplistDefaultPath = './config/idplist-default.json';
+  const idplistOverridePath = './config/idplist-override.json';
   return (await import(idplistOverridePath).catch(async e => {
     return await import(idplistDefaultPath);
   })).default;
