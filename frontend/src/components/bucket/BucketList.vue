00bee6193639196cacd01bb9f8023f9a3d6e16d0
Norris Ng <103449568+norrisng-bc@users.noreply.github.com>
2023-07-25 10:23:25 -0700

    Change "configure bucket" to "connect bucket"
    Emphasizes that an existing bucket is required

--- frontend/src/components/bucket/BucketList.vue
+++ frontend/src/components/bucket/BucketList.vue
@@ -65,7 +65,7 @@ onMounted(async () => {
         @click="showBucketConfig()"
       >
         <font-awesome-icon icon="fa-solid fa-plus" />
-        Configure bucket to BCBox
+        Connect bucket to BCBox
       </Button>
       <Dialog
         class="bcbox-info-dialog"
